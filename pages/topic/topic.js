f4df8b81feae171519c002f1b2cfd670c0fdf500
杨超 <yangchao@babyfs.cn>
2018-06-30 16:23:42 +0800

    init miniprogrem

---
+++ pages/topic/topic.js
@@ -0,0 +1,67 @@
+// topic/topic.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    question: "又一遍 我忘记了是哪些时间/n你言辞闪烁 原因当然不明显",
+    answers: ["富士山下", "最佳损友", "心的距离", "一丝不挂"]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
