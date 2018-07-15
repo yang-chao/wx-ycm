e1a9c5e3c9f596890f64a78544db406258f25848
杨超 <yangchao@babyfs.cn>
2018-07-15 21:36:08 +0800

    update ui of the topic page

---
+++ constants/api.js
@@ -0,0 +1,6 @@
+const base = "http://123.206.62.140/"
+const topic = base + "topic"
+
+module.exports = {
+  topic: topic
+}
--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -1,67 +1,91 @@
 // topic/topic.js
-Page({
+const api = require("../../constants/api.js")
+var that
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    question: "又一遍 我忘记了是哪些时间/n你言辞闪烁 原因当然不明显",
-    answers: ["富士山下", "最佳损友", "心的距离", "一丝不挂"]
+    question: "",
+    answers: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    that = this
+    wx.request({
+      url: api.topic,
+      method: 'GET',
+      success: function(res) {
+        console.log(res.data)
+        var answers = res.data.answer
+        var index
+        for (var i = 0; i < answers.length; i++) {
+          answers[i] = (i + 1) + '. ' + answers[i]
+        }
+        that.setData({
+          question: res.data.question,
+          answers: res.data.answer
+        })
+      },
+      fail: function() {
+        console.log('fail')
+      },
+      complete: function() {
+        console.log('complete')
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
